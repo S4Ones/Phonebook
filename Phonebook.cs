7753e48026ab683eb86b8f9a0e2f9439d1e55d3d
S4Ones <S4Ones@DESKTOP-UHQF63C>
2023-08-05 13:34:39 +0300

    Добавьте файлы проекта.

---
+++ Phonebook.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    internal class Phonebook
+    {
+        private static Phonebook instance;
+        private List<Abonent> abonents;
+        private const string filePath = "phonebook.txt";
+
+        /// <summary>
+        /// Подгрузка телефонного списка.
+        /// </summary>
+        public Phonebook() 
+        { 
+            abonents = new List<Abonent>();
+            LoadData();
+        }
+
+        /// <summary>
+        /// Реализация синглтона.
+        /// </summary>
+        public static Phonebook Instance
+        {
+            get
+            {
+                if(instance == null)
+                    instance = new Phonebook();
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Добавление абонента в телефонный список.
+        /// </summary>
+        /// <param name="name">Имя абонента</param>
+        /// <param name="phone">Номер телефона абонента.</param>
+        public void AddPhonebook(string name, string phone)
+        {
+            if(abonents.Any(ab => ab.Name == name || ab.Phone == phone))
+            {
+                Console.WriteLine("Абонент уже существует.");
+                return;
+            }
+
+            Abonent newAbonent = new Abonent()
+            {
+                Name = name,
+                Phone = phone
+            };
+            abonents.Add(newAbonent);
+            SaveData();
+            Console.WriteLine("Абонент успешно добавлен.");
+        }
+
+        /// <summary>
+        /// Получение имени абонента при помощи его номера телефона.
+        /// </summary>
+        /// <param name="phone">Номер телефона абонента.</param>
+        public void GetAbonentByPhone(string phone)
+        {
+            var name = abonents.FirstOrDefault(ab => ab.Phone == phone);
+            Console.Write("Имя абонента: ");
+            Console.WriteLine(name != null ? name.Name : null);
+        }
+
+        /// <summary>
+        /// Получене номера телефона абонента при помощи его имени.
+        /// </summary>
+        /// <param name="name">Имя абонента.</param>
+        public void GetAbonentByName(string name)
+        {
+            var abonent = abonents.FirstOrDefault(ab => ab.Name == name);
+            Console.Write("Телефон абонента: ");
+            Console.WriteLine(abonent != null ? abonent.Phone : null);
+        }
+
+        /// <summary>
+        /// Удаление абонента из телефонного списка.
+        /// </summary>
+        /// <param name="phone">Номер телефона абонента.</param>
+        public void RemoveContact(string phone)
+        {
+            Abonent abonentToRemove = abonents.FirstOrDefault(a => a.Phone == phone);
+            if (abonentToRemove != null)
+            {
+                abonents.Remove(abonentToRemove);
+                SaveData();
+                Console.WriteLine("Абонент успешно удален.");
+            }
+            else
+            {
+                Console.WriteLine("Абонент с таким номером телефона не найден.");
+            }
+        }
+
+        /// <summary>
+        /// Сохранение телефонного списака в файле.
+        /// </summary>
+        private void SaveData()
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var abonent in abonents)
+                {
+                    writer.WriteLine($"{abonent.Phone},{abonent.Name}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Загрузка телефонного списака из файла.
+        /// </summary>
+        private void LoadData()
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length == 2)
+                        {
+                            Abonent abonent = new Abonent
+                            {
+                                Name = parts[0],
+                                Phone = parts[1]
+                            };
+                            abonents.Add(abonent);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Phonebook
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Главное меню.
+        /// </summary>
+        static void Main()
+        {
+            Phonebook phonebook = Phonebook.Instance;
+            Phonebook contact = new Phonebook();
+
+            while(true)
+            {
+                Console.WriteLine("Выберете действие.");
+                Console.WriteLine("1. Добавить новый контакт.");
+                Console.WriteLine("2. Получить абонента по номеру телефона.");
+                Console.WriteLine("3. Получить номер телефона по имени абонента.");
+                Console.WriteLine("4. Удалить контакт.");
+                Console.WriteLine("5. Выйти.");
+
+                string choice = Console.ReadLine();
+                switch(choice)
+                {
+                    case "1":
+                        AddAbonent(contact);
+                        break;
+                    case "2":
+                        GetAbonentByPhone(contact);
+                        break;
+                    case "3":
+                        GetAbonentByName(contact);
+                        break;
+                    case "4":
+                        RemoveContact(contact);
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Ошибка: Недопустимый выбор.");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Заполнение данных абонента для добавления в список контактов.
+        /// </summary>
+        /// <param name="contact">Телефонный список.</param>
+        static void AddAbonent(Phonebook contact)
+        {
+            Console.Write("Введите имя абонента: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите номер абонента: ");
+            string phone = Console.ReadLine();
+            contact.AddPhonebook(name, phone);
+        }
+
+
+        /// <summary>
+        /// Заполнение данных абонента для его поиска с помошью номера телефона.
+        /// </summary>
+        /// <param name="contact">Телефонный список.</param>
+        static void GetAbonentByPhone(Phonebook contact)
+        {
+            Console.Write("Введите номер абонента: ");
+            string phone = Console.ReadLine();
+
+            contact.GetAbonentByPhone(phone);
+        }
+
+        /// <summary>
+        /// Заполнение данных абонента для поиска его номера телефона с помошью имени.
+        /// </summary>
+        /// <param name="contact">Телефонный список.</param>
+        static void GetAbonentByName(Phonebook contact)
+        {
+            Console.Write("Введите имя абонента:");
+            string name = Console.ReadLine();
+
+            contact.GetAbonentByName(name);
+        }
+
+        /// <summary>
+        /// Заполнение данных абонента для его удаления из телефонной книги.
+        /// </summary>
+        /// <param name="contact">Телефонный список.</param>
+        static void RemoveContact(Phonebook contact)
+        {
+            Console.Write("Введите номер абонента: ");
+            string phone = Console.ReadLine();
+
+            contact.RemoveContact(phone);
+        }
+    }
+}
